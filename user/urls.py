38bba31366a30c6a649426a629e792050a3fb196
deokmin kim <kdm006@naver.com>
2023-08-23 10:18:21 +0900

    로그인기능 수정

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('login/', views.sign_in, name='login'),
     path('logout/', views.sign_out, name='logout'),
     path('register/', views.sign_up, name='register'),
+
 ]
--- user/views.py
+++ user/views.py
@@ -13,9 +13,9 @@ def sign_up(request):
             user = form.save(commit=False) # 바로 저장하지 않고
             user.username = user.username.lower() # username 들어오면 이름을 소문자로 바꾸어서
             user.save() # 이름 저장
-            messages.success(request, "U' have Signed up successfully.")
+            messages.success(request, "You have Signed up successfully.")
             login(request,user)
-            return redirect('home')
+            return redirect('/')
         else:
             return render(request, 'user/register.html', {'form': form})
 
@@ -33,7 +33,7 @@ def sign_in(request):
                 login(request, user)
                 messages.success(
                     request, f'Hi {username.title()}, welcome back!')
-                return redirect('home')
+                return redirect('user:home')
 
         # 유효하지 않거나 사용자가 인증 되지 않을 경우
         messages.error(request, f'Invalid username or password')
@@ -42,5 +42,8 @@ def sign_in(request):
 def sign_out(request):
     logout(request)
     messages.success(request, f'You have been logged out.')
-    return redirect('')
+    return redirect('user:home')
 
+
+def home(request):
+    return render(request, 'product/index.html')
