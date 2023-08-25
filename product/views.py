42306276624398a605fe8b92e0620214236d8444
deokmin kim <kdm006@naver.com>
2023-08-25 13:27:53 +0900

    shop.html paging기능 추가, login/register이미지 변경

--- product/views.py
+++ product/views.py
@@ -3,7 +3,7 @@ from .models import Product
 from django.db.models import Q
 from .forms import ProductForm
 from django.contrib import messages
-
+from django.core.paginator import Paginator
 
 def main_page(request): # main_page 함수는 웹사이트의 메인 페이지를 렌더링하는 부분입니다.
     # products = Product.objects.all()
@@ -25,7 +25,12 @@ def contact_page(request):
 
 def shop_page(request):  # product_list 함수는 모든 상품을 리스트 형태로 렌더링하는 부분입니다.
     products = Product.objects.all()
-    return render(request, 'product/shop.html', {'products': products})
+    items_per_page = 15  # 한 페이지에 보여줄 항목 수
+    paginator = Paginator(products, items_per_page)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    current_page = page.number
+    return render(request, 'product/shop.html', {'products': products, 'page': page, 'current_page': current_page})
     # Product.objects.all()은 데이터베이스에서 모든 Product 객체를 가져오는 쿼리를 수행합니다.
     # 이렇게 가져온 상품 객체들은 'product/shop_page.html' 템플릿에 'products'라는 이름으로 전달되어 렌더링됩니다.
 
